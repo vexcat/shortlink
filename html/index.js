2aca1e59b8658bc50ea5b9d63d57555f890bd091
Gabriel Pulido <autodeskgato@gmail.com>
2019-04-14 12:47:35 -0700

    Commit local files to new git repo

---
+++ html/index.js
@@ -0,0 +1,39 @@
+$('#make-a-link input').first().focus();
+$.ajax({
+  url: '/listing',
+  dataType: 'json'
+}).done(data => {
+  const links = $('#active-links');
+  const template = $('.link-template').first();
+  const keys = Object.keys(data).sort((a, b) => a < b ? -1 : 1);
+  for(let link of keys) {
+    let clone = template.clone().show();
+    clone.find('.link-display-from').attr('href', 'http://i/' + link).text('i/' + link);
+    clone.find('.link-display-to').attr('href', data[link]).text(data[link]);
+    clone.find('.action-delete-link').click(function() {
+      const form = $(`<form action="/" method="POST">
+      <input type="text" name="action" value="delete">
+      <input type="text" name="from">
+      <input type="submit">
+      </form>`);
+      $('body').append(form);
+      form.find('input').eq(1).val(link);
+      form.hide();
+      form.submit();
+    });
+    clone.find('.action-edit-link').click(function() {
+      const editor = $('.link-editor-template');
+      clone.hide();
+      editor.detach().insertAfter(clone).show();
+      editor.find('.editor-previous-name').val(link);
+      editor.find('.editor-from').val(link);
+      editor.find('.editor-to').val(data[link]).focus().select();
+    });
+    links.append(clone);
+  }
+  links.show();
+});
+
+function editorclose() {
+  $('.link-editor-template').hide().prev().show();
+}
---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const port = 80;
+const fs = require('fs');
+const data = JSON.parse(fs.readFileSync('uwu.json', 'UTF-8'));
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.urlencoded({
+  type: () => true
+}));
+
+app.use(express.static('html'));
+
+app.get('/listing', (req, res) => {
+  res.send(data);
+});
+
+function options(short) {
+  return Object.entries(data).filter(pair => {
+    const link = pair[0];
+    return link.startsWith(short);
+  });
+}
+
+app.get('/options', (req, res) => {
+  res.status(300).sendFile(__dirname + '/html/options.html');
+})
+
+app.get('/options/:link', (req, res) => {
+  res.send(JSON.stringify(options(req.params.link)));
+})
+
+app.get('/:link', (req, res) => {
+  const pairs = options(req.params.link);
+  if(pairs.length === 1) {
+    res.redirect(pairs[0][1]);
+  } else if(pairs.length >= 1) {
+    res.redirect('/options?' + encodeURIComponent(req.params.link));
+  } else {
+    res.send('No such link.');
+  }
+});
+
+function saveState() {
+  return new Promise((res, rej) => {
+    fs.writeFile('uwu.json', JSON.stringify(data), 'UTF-8', err => {
+      if(err) {
+        rej(err); return;
+      }
+      res();
+    });
+  });
+}
+
+app.post('/', async (req, res) => {
+  switch(req.body.action) {
+    case "create":
+    data[req.body.from] = req.body.to;
+    break;
+    case "change":
+    delete data[req.body['old-name']];
+    data[req.body.from] = req.body.to;
+    break;
+    case "delete":
+    delete data[req.body.from];
+    break;
+  }
+  await saveState();
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
